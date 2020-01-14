dc3e7b4b87bd11191e49e41b53ecd6729ee940a4
DaryaYa <lyalyaka@mail.ru>
2020-01-14 13:54:30 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = '' + n;
+    let strRev = str.split('').reverse().join('');
+    return Number.parseInt(strRev);
 }
